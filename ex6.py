c8f4eb46a48743985b2adb466edb1fd7d36fd881
liyinan <liyinan_coder@yeah.net>
2019-01-24 14:00:55 +0800

    fix

--- ex6.py
+++ ex6.py
@@ -1,18 +1,23 @@
 # -*- coding:utf-8 -*-
 from ex4 import ParseArgs
 import os
-import re
+import fnmatch
 
 
 class Find:
     def __init__(self):
         # 初始化参数
         self.args = ParseArgs()
-        self.args_len = self.args.parse_args()
-
+        args_len = self.args.parse_args()
+        if (args_len - 1 in self.args.flags
+            ) and self.args.flags[args_len - 1] == '--print':
+            self.print = lambda value: print(value)
+        else:
+            self.print = lambda value: value
         # 功能扩展
         self.switch = {
             'name': self._name,
+            'type': self._info,
         }
         self.func = []
         # 过滤
@@ -36,12 +41,13 @@ class Find:
         file_list = os.listdir(work_space)
         for file in file_list:
             file = os.path.join(work_space, file)
+            if self._exp(file):
+                self.print(file)
+                file_paths.append(file)
+
             if os.path.isdir(file):
                 self._push_file(file, file_paths)
                 continue
-            if self._exp(file):
-                print(file)
-                file_paths.append(file)
 
     def _exp(self, file):
         for fun in self.func:
@@ -51,7 +57,17 @@ class Find:
         return True
 
     def _name(self, filter):
-        return lambda f: True if re.search(filter, os.path.split(f)[1]) is not None else False
+        return lambda f: fnmatch.fnmatch(os.path.split(f)[-1], filter)
+
+    def _info(self, filter):
+        type_switch = {
+            'd': os.path.isdir,
+            'f': os.path.isfile,
+            'l': os.path.islink,
+        }
+        if not filter in type_switch:
+            raise Exception('unrecognized type')
+        return lambda f: True if type_switch[filter](f) is True else False
 
 
 if __name__ == "__main__":

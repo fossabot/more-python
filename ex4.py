543574c851c2dd9bc74aa2f76fabff5d983f65e8
liyinan <liyinan_coder@yeah.net>
2019-01-24 12:03:58 +0800

    format

--- ex4.py
+++ ex4.py
@@ -4,16 +4,18 @@ import sys
 
 # 基于sys.args的参数解析
 
+
 class ParseArgs:
     flags = {}
     select = {}
     l = 0
+
     def parse_args(self):
         args = sys.argv
         l = len(sys.argv) - 1
         if len(args) == 2 and (args[1] == '-h' or args[1] == '--help'):
             print('-h, --help 获得帮助文档')
-            return 
+            return
         select_title = ''
         index = -1
         for item in args[1:]:
@@ -27,8 +29,10 @@ class ParseArgs:
                 continue
             self.flags[index] = item
         if select_title != '':
-            self.flags[index] = '--' + select_title if len(select_title) > 1 else '-' + select_title
+            self.flags[index] = '--' + select_title if len(
+                select_title) > 1 else '-' + select_title
         return l
+
 
 if __name__ == "__main__":
     args = ParseArgs()
